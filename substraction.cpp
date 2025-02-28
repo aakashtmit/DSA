568d794347f463c2b0a7b49b83558fa7839a41d2
Aakash Sanjay Tambe <aakashtmit@gmail.com>
2025-02-28 07:14:07 +0530

    add new files

---
+++ substraction.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2, difference;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Subtract the two numbers
+    difference = num1 - num2;
+
+    cout << "The difference is: " << difference << endl;
+
+    return 0;
+}
